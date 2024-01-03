cf8a0d09a469ecfc86f7153c1f92543b61798a9e
= <djonathan.goulart@gmail.com>
2024-01-02 22:34:28 -0300

    remove LT function from spacebar

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/goulartdev/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/goulartdev/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(FN, KC_TAB),    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
     LT(FN1, KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,       KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
-        KC_LGUI,       KC_LALT,  KC_LCTL,                                LT(FN, KC_SPC),                         KC_RGUI,  TT(FN),   TT(FN1),  KC_ALGR),
+        KC_LGUI,       KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RGUI,  TT(FN),   TT(FN1),  KC_ALGR),
 
     [PC] = LAYOUT_61_ansi(
         _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
