6dad44e67e851ca1c497de8d429081f53718e4eb
Djonathan Goulart <contact@djonathan.dev>
2024-02-09 23:52:39 -0300

    remove mac layer

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/goulartdev/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/goulartdev/keymap.c
@@ -19,6 +19,7 @@
 enum layers{
     MAC,
     PC,
+//    MAC2,
     FN,
     FN1,
 };
@@ -30,21 +31,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(FN, KC_TAB),    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
     LT(FN1, KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,       KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                      KC_RSFT,
-        KC_LGUI,       KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RGUI,  TT(FN),   TT(FN1),  KC_ALGR),
+        KC_LCTL,       KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RGUI,  TT(FN),   TT(FN1),  KC_ALGR),
 
     [PC] = LAYOUT_61_ansi(
         _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
-        KC_LCTL,       _______,  KC_LGUI,                                _______,                                _______,  _______,  _______,  _______),
+        _______,       _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+
+//    [MAC2] = LAYOUT_61_ansi(
+//        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+//        KC_TAB,        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+//        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+//        _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
+//        _______,       _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
     [FN] = LAYOUT_61_ansi(
         _______,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_BSPC,
         _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_PGDN,  _______,
         _______,       _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_HOME,  KC_END,             _______,
         _______,       _______,  KC_DEL,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
-    LM(FN, MOD_LCTL),  _______,  LM(FN, MOD_LGUI),                       _______,                                _______,  _______,  _______,  _______),
+        _______,       _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
     [FN1] = LAYOUT_61_ansi(
         _______,       KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPLY,  KC_MPRV,  KC_MNXT,  KC_BRID,  KC_BRIU,  XXXXXXX,  XXXXXXX,  KC_SLEP,  KC_PWR,   BAT_LVL,
@@ -54,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,       _______,  _______,                                BAT_LVL,                                _______,  _______,  _______,  _______),
     };
 
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-const key_override_t acute_esc_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_ESC, KC_GRV);
-const key_override_t tilde_esc_override = ko_make_basic(MOD_BIT(KC_RGUI), KC_ESC, S(KC_GRV));
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL); // shift + bdpc = del
+const key_override_t acute_esc_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_ESC, KC_GRV); // rshift + esc = grave
+const key_override_t tilde_esc_override = ko_make_basic(MOD_BIT(KC_RGUI), KC_ESC, S(KC_GRV)); // rgui + esc = tilde
 
-const key_override_t vim_x_bspc_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DEL, KC_BSPC, 1<<FN);
+const key_override_t vim_x_bspc_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DEL, KC_BSPC, 1<<FN); // vim x = del / X = bspc
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &delete_key_override,
@@ -67,3 +75,37 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &vim_x_bspc_override,
     NULL
 };
+
+//bool swap_lgui_lctl = false;
+//uint8_t mod_state;
+//uint8_t MOD_MASK_LCLG = MOD_BIT(KC_LGUI) | MOD_BIT(KC_LCTL);
+
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//    mod_state = get_mods();
+//
+//    switch (keycode) {
+//        case KC_TAB:
+//            if (record->event.pressed) {
+//                if (IS_LAYER_OFF(PC) && !swap_lgui_lctl) {
+//                    // swap LGUI <-> LCTRL on MAC layer when pressed together with TAB
+//                    if ((mod_state & MOD_MASK_LCLG) && (mod_state & ~MOD_MASK_SHIFT) == (mod_state & MOD_MASK_LCLG)) {
+//                        unregister_mods(mod_state & MOD_MASK_LCLG);
+//                        register_mods(mod_state ^ MOD_MASK_LCLG);
+//                        swap_lgui_lctl = true;
+//                    }
+//                }
+//            }
+//            break;
+//    }
+//    return true;
+//}
+
+
+//layer_state_t layer_state_set_user(layer_state_t state) {
+//    if (swap_lgui_lctl && IS_LAYER_OFF_STATE(state, MAC2)) {
+//        unregister_mods(get_mods() & MOD_MASK_LCLG);
+//        swap_lgui_lctl = false;
+//    }
+//
+//    return state;
+//}
