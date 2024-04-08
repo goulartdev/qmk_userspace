31e27179a70a0195df119abee12a86727300e198
Djonathan Goulart <contact@djonathan.dev>
2024-04-07 21:35:46 -0300

    add home row modifiers

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/goulartdev/config.h
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/goulartdev/config.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #define TAPPING_TOGGLE 2
+
+// https://precondition.github.io/home-row-mods
+// Left-hand home row mods
+#define A_GUI LGUI_T(KC_A)
+#define S_ALT LALT_T(KC_S)
+#define D_SFT LSFT_T(KC_D)
+#define F_CTL LCTL_T(KC_F)
+#define C_ALGR ALGR_T(KC_C)
+
+// Right-hand home row mods
+#define J_CTL RCTL_T(KC_J)
+#define K_SFT RSFT_T(KC_K)
+#define L_ALT LALT_T(KC_L)
+#define SCLN_GUI RGUI_T(KC_SCLN)
+#define M_ALGR ALGR_T(KC_M)
